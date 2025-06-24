060761b3df7a0533d5d9d0ac269aa7211e095256
Mădălina Grădinaru <140403978+magdalenagradinaru@users.noreply.github.com>
2025-06-24 18:53:52 +0300

    Update generative_module.py

--- chatbot_backend/generative_module.py
+++ chatbot_backend/generative_module.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "sk..."
 
 def get_gpt_response(user_input):
     try:
